b29964ec2811c62448bc2e86befea698e68f3ab1
NoobyMcNoobNoobson <dhakalrijan825@uah.edu>
2021-02-24 12:57:30 -0600

    This is a change that I am making

--- 1.py
+++ 1.py
@@ -2,5 +2,7 @@ def main():# this is the program that will call the main program
     print("I am just testing git here")
     print("Why is the code not showing up")
 
+    print("This is from the desktop in shelby and I just want to see if this will show up")
+
 
 main()
