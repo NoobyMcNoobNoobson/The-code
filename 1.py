5adb0429d5f473f1ad03f70a7e1f6497b44a4fae
NoobyMcNoobNoobson <dhakalrijan825@uah.edu>
2021-02-24 14:54:17 -0600

    from the desktop

--- 1.py
+++ 1.py
@@ -1,9 +1,5 @@
 def main():# This is the program that will pull all the other required functions together
-    print('This should show up on there')
-
-    print("This is from the desktop")
-
-    print("From the laptop")
+    
     
 main()
 
