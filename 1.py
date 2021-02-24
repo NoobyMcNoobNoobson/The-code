1028d82b9b36b68f5d94a991714deb034fdb475c
NoobyMcNoobNoobson <dhakalrijan825@uah.edu>
2021-02-24 12:58:51 -0600

    Will this work by any chance

--- 1.py
+++ 1.py
@@ -4,5 +4,7 @@ def main():# this is the program that will call the main program
 
     print("This is from the desktop in shelby and I just want to see if this will show up")
 
+    print("For some reason this is not working at all, or at least in the way that I want it to ")
+
 
 main()
