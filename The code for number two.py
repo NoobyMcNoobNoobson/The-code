acf6226050a2438e53e6dba545bcc3cd4c2e4d91
NoobyMcNoobNoobson <dhakalrijan825@gmail.com>
2021-03-01 14:55:36 -0600

    laptop

--- The code for number two.py
+++ The code for number two.py
@@ -3,11 +3,15 @@ from pandas import DataFrame
 
 print ('pandas version: ' + pd.__version__)
 
+#C:\\Users\\dhaka\\OneDrive\\Desktop\\Semester material\\Data Skills class\\All Homework\\three\\The code\\The-code
+
 import os
+from os import path
 
 def main():
     the_size()
 
 def the_size():
     os.chdir('C:\\Users\\dhaka\\OneDrive\\Desktop\\Semester material\\Data Skills class\\All Homework\\three\\The code\\The-code')
+    data_sample = open('C:\\Users\\dhaka\\OneDrive\\Desktop\\Semester material\\Data Skills class\\All Homework\\three\\The code\\The-code')
     
