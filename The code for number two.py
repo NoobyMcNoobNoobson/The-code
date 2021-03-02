3e56ffa1d1cebd490f39eb97f68cf9b55aba78d6
NoobyMcNoobNoobson <dhakalrijan825@uah.edu>
2021-03-02 17:49:00 -0600

    desktop

--- The code for number two.py
+++ The code for number two.py
@@ -28,7 +28,7 @@ def mean_hindfoot_lenght():
 def mean_plot():
     data_sample = pd.read_csv('surveys.csv') # open the file as needed 
     print('The mean weight by plot for is as follows:-') # this is just to make the printout look a bit more natural
-    groupby_plot = data_sample.groupby(['plot'])['weight'].mean()
+    groupby_plot = data_sample.groupby(['plot_id'])['weight'].mean()
 
 
 main()
