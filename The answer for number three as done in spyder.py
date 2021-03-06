8a175d5a2782e59a13c8bfe34ae67ed57c1170b5
NoobyMcNoobNoobson <dhakalrijan825@gmail.com>
2021-03-05 21:47:17 -0600

    laptop

--- The answer for number three as done in spyder.py
+++ The answer for number three as done in spyder.py
@@ -1,5 +1,5 @@
 import pandas as pd
 from pandas import DataFrame
-#import matplotlib as mpl
+import matplotlib as mpl
 print ('pandas version: ' + pd.__version__)
-#print('matplotlib version: '+ mpl.__version__)
+print('matplotlib version: '+ mpl.__version__)
