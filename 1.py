7b3f7fd454de30d9dbc5989dd1d7f9e98755daa5
NoobyMcNoobNoobson <dhakalrijan825@gmail.com>
2021-03-05 19:49:27 -0600

    laptop

--- 1.py
+++ 1.py
@@ -1,3 +1,6 @@
+# Dear Paul, I was hoping to maybe get some extra points doing this but if you see this message at the top then probably don't bother running this.
+# I was planning to do the json in python after all the other required ones.
+
 def main():# This is the program that will pull all the other required functions together
     print('This should be the code on the website')
     
