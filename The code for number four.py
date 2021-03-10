bf0c2cc92df3732653091da69a85e55f84462a63
NoobyMcNoobNoobson <dhakalrijan825@gmail.com>
2021-03-09 20:26:10 -0600

    laptop

--- The code for number four.py
+++ The code for number four.py
@@ -11,12 +11,12 @@ def main():
     #left_merge.to_csv('../left_merge.csv') if you uncomment this out then you can see if you did the merge right 
     filtered_list_of_species = left_merge[left_merge['genus'].isin(['Dipodomys','Sigmodon','Chaetodipus'])]
     #this will write the genus filtered data frame to a csv file
-    filtered_list_of_species.to_csv('../filered_list_of_species.csv')
+    filtered_list_of_species.to_csv('../filered_list_of_species.csv') # print the filtered list to a csv file in the main directory
     print('The shape of the filtered list of species is as follows')
     print(filtered_list_of_species.shape)
 
-    print('The mean weight of each sec in each month is as follows:-')
-    groupby_gender_for_weight = filtered_list_of_species.groupby(['sex','genus'])['weight'].mean()
+    print('The mean weight of each sex in each month is as follows:-')
+    groupby_gender_for_weight = filtered_list_of_species.groupby(['sex','genus'])['weight'].mean() # the combination for the mean weight of each sex in each genus
     print(groupby_gender_for_weight)
 
 main()
